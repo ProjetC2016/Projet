143cb15ac5dffdb2fd4af3553090664d736c4461
BettyBismuth <betty.bismuth@hotmail.fr>
2016-12-08 20:53:07 +0100

    first commit

---
+++ Projet/tchatche.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+
+void createPipe();
